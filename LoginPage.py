a9640dbd6adff0acf7647dcd45efc15404f40c14
Wyy-96 <ping_996@qq.com>
2022-08-17 09:15:10 +0800

    使用ttk，应用主题样式

--- LoginPage.py
+++ LoginPage.py
@@ -32,14 +32,16 @@ class LoginPage(object):
         
         self.page = Frame(self.root,background="")  # 创建Frame
         self.page.place(x=self.width/2-100, y=self.height/2-100)
-        Label(self.page,text="登录").grid(row=0, stick=W)
-        Label(self.page, text='账户：').grid(row=1, stick=W, pady=10)
-        Entry(self.page, textvariable=self.username).grid(row=1, column=1, stick=E)
-        Label(self.page, text='密码：').grid(row=2, stick=W, pady=10)
-        Entry(self.page, textvariable=self.password, show='*').grid(row=2, column=1, stick=E)
-        Button(self.page, text='登录', command=self.loginCheck).grid(row=3, stick=W, pady=10)
-        Button(self.page, text='注册', command=self.register).grid(row=3, column=2,stick=W, pady=10)
-        Button(self.page, text='退出', command=self.page.quit).grid(row=3, column=3, stick=E)
+        ttk.Label(self.page,text="登录").grid(row=0, stick=W)
+        ttk.Label(self.page, text='账户：').grid(row=1, stick=W, pady=10)
+
+        # Entry
+        ttk.Entry(self.page, textvariable=self.username).grid(row=1, column=1, stick=E)
+        ttk.Label(self.page, text='密码：').grid(row=2, stick=W, pady=10)
+        ttk.Entry(self.page, textvariable=self.password, show='*').grid(row=2, column=1, stick=E)
+        ttk.Button(self.page, text='登录', command=self.loginCheck).grid(row=3, stick=W, pady=10)
+        ttk.Button(self.page, text='注册', command=self.register).grid(row=3, column=2,stick=W, pady=10)
+        ttk.Button(self.page, text='退出', command=self.page.quit).grid(row=3, column=3, stick=E)
         
 
     def loginCheck(self):
